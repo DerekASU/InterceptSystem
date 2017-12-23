6ae1e0a7f8c913df4b23024e4b390455ca67c84b
wmurray919 <wmurray919@gmail.com>
2017-12-23 12:37:17 -0700

    documentationupdate

--- MDSCSSDemo/src/mdscssControl/ControlThread.java
+++ MDSCSSDemo/src/mdscssControl/ControlThread.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 public class ControlThread implements Runnable
 {
     private MDSCSSController controlUtility;
-    private boolean bRunning;
+    private boolean bRunning, bFsPeriod;
     private controlState threadState;
     
     private enum controlState
@@ -37,17 +37,15 @@ public class ControlThread implements Runnable
     /***************************************************************************
      * Run
      * 
-     * TODO::Add description
+     * Main control thread.  Handles control flow to initialize the controller
+     * and maintain connections and operability with the subsystems.
      **************************************************************************/
     public void run()
     {
+        long entryTime = 0, elapsedTime = 0;
         bRunning = true;
-        long entryTime = 0;
-        long elapsedTime = 0;
-        boolean everyother = false;
-        boolean active = false;
+        bFsPeriod = false;
 
-                
         System.out.println("ControlThread: Thread Started");
         
         try 
@@ -56,70 +54,77 @@ public class ControlThread implements Runnable
             {
                entryTime = System.currentTimeMillis();
                
-               
                switch(threadState)
                {
                    case initializing: 
-                       
+                       // Establish TCP connections
                        if(controlUtility.establishConnection())
                        {
                            threadState = controlState.activating;
                        }
+                       // Check for socket failures, and handle 5 minute connection timeout
                        else
                        {
                            controlUtility.checkForFailure();
-
                        }
                            
+                       //endure that the watchdog is disabled until all subsystems are connected
                        controlUtility.disableWatchdog();
                        
-
-
+                       //Update operator with connection status
                        controlUtility.establishStatus();
-
-                       
                        break;
                    case activating: 
+                       // populate the internal database with missiles
                        controlUtility.initializeModel();
+                       
+                       // start the watchdog
                        controlUtility.initializeWatchdog();
 
                        threadState = controlState.operational;
-                       
                        break;
                    case operational: 
-                     
+                       // ping the watchdog
                        controlUtility.handleWatchdogTimer();
+                       // update the database with updated information
                        controlUtility.updateModel();
+                       // respond to operator interaction
                        controlUtility.handleControlLogic();
                        
-                        if(everyother)
+                       // the PID controller has been tuned to an Fs value of 1
+                       // while this thread wakes up every .5 seconds, to handle this
+                       // PID updates and thrust commands are gated to only send every
+                       // second
+                        if(bFsPeriod)
                         {
                             controlUtility.handleThrustControl();
-                            everyother = false;
+                            bFsPeriod = false;
                         }
                         else
-                            everyother = true;
+                        {
+                            bFsPeriod = true;
+                        }
 
-
-                       
-                      
                        break;
-               }
+                }
                
-                 elapsedTime = System.currentTimeMillis() - entryTime;
-                        if(elapsedTime < 500)
-                        {
-                             Thread.sleep(500 - elapsedTime);
-                        }
-                        else if(elapsedTime > 1000)
-                        {
-                            System.out.println("ControlThread -- Warning: latency time approaching 1 second [" + elapsedTime + "]");
-                        }
+                // command the thread to sleep so that it wakes up exactly a half second from
+                // the last time it awoke
+                elapsedTime = System.currentTimeMillis() - entryTime;
+                
+                if(elapsedTime < 500)
+                {
+                     Thread.sleep(500 - elapsedTime);
+                }
+                else if(elapsedTime > 1000)
+                {
+                    System.out.println("ControlThread -- Warning: latency time approaching 1 second [" + elapsedTime + "]");
+                }
             }
         } 
         catch (InterruptedException e) 
         {
-            System.out.print("ControlThread -- interrupted: stopping thread");
+            System.out.print("ControlThread: interrupted: stopping thread");
             bRunning = false;
         }
     }
